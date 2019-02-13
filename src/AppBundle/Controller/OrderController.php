1a32fd0af9d2320ac41cc90eb369f9949a633d8d
Manu <jmgonzalez@hola.com>
2019-02-13 16:13:02 +0100

    Charge it ( the stripe PHP SDK ) episode (chapter 1)

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -35,7 +35,18 @@ class OrderController extends BaseController
         if ($request->isMethod('POST')) {
             $token = $request->request->get('stripeToken');
 
-            dump($token);
+           \Stripe\Stripe::setApiKey('sk_test_VkzNFwZFoPLT7YCYQODWwXB9');
+           \Stripe\Charge::create([
+                "amount" => $this->get('shopping_cart')->getTotal() * 100,
+                "currency" => "usd",
+                "source" => $token,
+                "description" => "First test charge!"
+           ]);
+
+           $this->get('shopping_cart')->emptyCart();
+           $this->addFlash('success', 'Order Complete! Yay!');
+
+           return $this->redirectToRoute('homepage');
         }
 
         return $this->render('order/checkout.html.twig', array(
